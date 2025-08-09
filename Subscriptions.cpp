272b2274a8ecc58ec9f158ccd5912f1a4f6e3655
Kazi Saif Hossain Emon <133599101+kazisaif23@users.noreply.github.com>
2025-08-09 20:49:05 +0600

    SUBSCRIBE_

---
+++ Subscriptions.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastread()      (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vvi = vector<vi>;
+using vvll = vector<vll>;
+
+void solve() {
+    ll n, x;
+    cin >> n >> x;
+    ll line = ceil(n / 6.00);
+    ll cost = line * x;
+    cout << cost << endl;
+
+    
+}
+
+int main() {
+    fastread();
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+    solve();
+     }
+    return 0;
+}
